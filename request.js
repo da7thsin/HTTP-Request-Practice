1559316ad016818bffb5d810703d61dbc64329dd
Geoffrey <geo.galagaran@gmail.com>
2017-01-09 11:28:44 +0800

    Slight Change

--- request.js
+++ request.js
@@ -1,1 +1,7 @@
-alert('working');
+var req = new XMLHttpRequest();
+req.open("GET", "data.json");
+req.addEventListener("load", function(){
+  console.log("Done", req.status);
+  console.log(JSON.parse(req.responseText));
+});
+req.send();
