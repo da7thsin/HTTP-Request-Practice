69aef2ea9302b3a21a0981b22ded6979259d8a66
Geoffrey <geo.galagaran@gmail.com>
2017-01-09 22:28:50 +0800

    Changed

--- request.js
+++ request.js
@@ -1,39 +1,58 @@
-function getURL(url, callback){
-  var req = new XMLHttpRequest();
+var helper = (function(){
+  function getURL(url, callback){
+    var req = new XMLHttpRequest();
 
-  function loadAction(){
-    if(req.status == 200){
-      callback(req.responseText);
+    function loadAction(){
+      if(req.status == 200){
+        callback(req.responseText);
+      }
     }
+
+    if(/json/gi.test(url)){
+      req.overrideMimeType("application/json");
+    }
+
+    req.open("GET", url);
+    req.addEventListener("load", loadAction);
+    req.send();
   }
 
-  if(/json/gi.test(url)){
-    req.overrideMimeType("application/json");
+  function createItem(value){
+    var li = document.createElement("li");
+    var text = document.createTextNode(value);
+    li.setAttribute('class', 'item');
+    li.append(text);
+
+    return li;
   }
 
-  req.open("GET", url);
-  req.addEventListener("load", loadAction);
-  req.send();
-}
 
-function createItem(value){
-  var li = document.createElement("li");
-  var text = document.createTextNode(value);
-  li.setAttribute('class', 'item');
-  li.append(text);
+  function showDataValue(){
 
-  return li;
-}
+  }
+
+  return {
+    createItem: createItem,
+    showDataValue: showDataValue
+  }
+})();
 
 
-var app = (function(){
+(function(){
   var btn = document.querySelector("button");
   var input = document.querySelector("input[type=text]");
   var ul = document.querySelector(".list-box");
 
   btn.addEventListener("click", function(){
     if(input.value){
-      ul.append(createItem(input.value));
+      ul.append(helper.createItem(input.value));
+      input.value = "";
+    }
+  });
+
+  input.addEventListener("keydown", function(event){
+    if(event.which === 13 && input.value){
+      ul.append(helper.createItem(input.value));
       input.value = "";
     }
   });
